09ff4645c545e09e21f83a122f487d35191e3b8d
Jatin Srivastava <137695680+JatinSri1909@users.noreply.github.com>
2024-06-04 06:07:02 +0530

    first commit

---
+++ app/(root)/transformations/[id]/update/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const UpdateTransformationPage = () => {
+  return (
+    <div>
+      UpdateTranformationPage
+    </div>
+  )
+}
+
+export default UpdateTransformationPage
