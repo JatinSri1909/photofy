1612a16d3ed17954ee38fdc566e12bd344b3f4f6
Jatin Srivastava <137695680+JatinSri1909@users.noreply.github.com>
2024-10-10 01:07:16 +0530

    created Header component

--- app/(root)/transformations/add/[type]/page.tsx
+++ app/(root)/transformations/add/[type]/page.tsx
@@ -1,10 +1,9 @@
 import React from 'react'
+import Header from '@/components/shared/Header'
 
 const AddTransformationTypePage = () => {
   return (
-    <div>
-      AddTransformationPage
-    </div>
+    <Header />
   )
 }
 
---
+++ components/shared/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+      Add Image Transfomration
+    </div>
+  )
+}
+
+export default Header
