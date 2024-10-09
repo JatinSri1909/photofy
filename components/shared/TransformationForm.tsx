73d4e2a0238dbd0cde8aca7a683e4dbe2de5b770
Jatin Srivastava <137695680+JatinSri1909@users.noreply.github.com>
2024-10-10 02:36:03 +0530

    Refactor TransformationForm component and add authentication check in AddTransformationTypePage

--- components/shared/TransformationForm.tsx
+++ components/shared/TransformationForm.tsx
@@ -1,10 +1,10 @@
-"use client"
- 
-import { zodResolver } from "@hookform/resolvers/zod"
-import { useForm } from "react-hook-form"
-import { z } from "zod"
+"use client";
 
-import { Button } from "@/components/ui/button"
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+import { Button } from "@/components/ui/button";
 import {
   Form,
   FormControl,
@@ -13,51 +13,53 @@ import {
   FormItem,
   FormLabel,
   FormMessage,
-} from "@/components/ui/form"
-import { Input } from "@/components/ui/input"
- 
-const formSchema = z.object({
-  username: z.string().min(2).max(50),
-})
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { title } from "process";
+import { defaultValues } from "@/constants";
 
-const TransformationForm = () => {
-   // 1. Define your form.
-   const form = useForm<z.infer<typeof formSchema>>({
+const formSchema = z.object({
+  title: z.string(),
+  aspectRatio: z.string().optional(),
+  color: z.string().optional(),
+  prompt: z.string().optional(),
+  publicId: z.string(),
+});
+
+const TransformationForm = ({
+  action,
+  data = null,
+}: TransformationFormProps) => {
+  const initialValues =
+    data && action === "Update"
+      ? {
+          title: data?.title,
+          aspectRatio: data?.aspectRatio,
+          color: data?.color,
+          prompt: data?.prompt,
+          publicId: data?.publicId,
+        }
+      : defaultValues;
+
+  // 1. Define your form.
+  const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
-    defaultValues: {
-      username: "",
-    },
-  })
- 
+    defaultValues: initialValues,
+  });
+
   // 2. Define a submit handler.
   function onSubmit(values: z.infer<typeof formSchema>) {
     // Do something with the form values.
     // ✅ This will be type-safe and validated.
-    console.log(values)
+    console.log(values);
   }
   return (
     <Form {...form}>
       <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-        <FormField
-          control={form.control}
-          name="username"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Username</FormLabel>
-              <FormControl>
-                <Input placeholder="shadcn" {...field} />
-              </FormControl>
-              <FormDescription>
-                This is your public display name.
-              </FormDescription>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <Button type="submit">Submit</Button>
+         
       </form>
     </Form>
-  )
-}
+  );
+};
 
-export default TransformationForm
+export default TransformationForm;
