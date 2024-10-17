984630a217cd966c6a3cfa88037e8f61dbf7aac2
Jatin Srivastava <137695680+JatinSri1909@users.noreply.github.com>
2024-10-18 00:42:23 +0530

    Refactor root page to import Collection component, add getAllImages function in image actions and add Search component

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,14 +1,20 @@
-import { Collection } from "@/components/shared/Collection";
-import { navLinks } from "@/constants";
-import Image from "next/image";
-import Link from "next/link";
+import { Collection } from "@/components/shared/Collection"
+import { navLinks } from "@/constants"
+import { getAllImages } from "@/lib/actions/image.actions"
+import Image from "next/image"
+import Link from "next/link"
 
-const Home = () => {
+const Home = async ({ searchParams }: SearchParamProps) => {
+  const page = Number(searchParams?.page) || 1;
+  const searchQuery = (searchParams?.query as string) || '';
+
+  const images = await getAllImages({ page, searchQuery})
+
   return (
     <>
       <section className="home">
         <h1 className="home-heading">
-          Unleash Your Creative Vision with Photofy
+          Unleash Your Creative Vision with Imaginify
         </h1>
         <ul className="flex-center w-full gap-20">
           {navLinks.slice(1, 5).map((link) => (
@@ -27,10 +33,15 @@ const Home = () => {
       </section>
 
       <section className="sm:mt-12">
-        <Collection images={[]} page={0} />
+        <Collection 
+          hasSearch={true}
+          images={images?.data}
+          totalPages={images?.totalPage}
+          page={page}
+        />
       </section>
     </>
-  );
-};
+  )
+}
 
-export default Home;
+export default Home
