7e4a32ef314d725a85704557354051160a265a70
Jatin Srivastava <137695680+JatinSri1909@users.noreply.github.com>
2024-10-29 14:59:07 +0530

    Refactor transaction action file to add checkoutCredits function and redirect to payment page

--- lib/actions/transaction.action.ts
+++ lib/actions/transaction.action.ts
@@ -1,9 +1,42 @@
 "use server";
 
 import Transaction from "../database/models/transaction.model";
+import Stripe from "stripe";
 import { connectToDatabase } from "../database/mongoose";
 import { handleError } from "../utils";
 import { updateCredits } from "./user.actions";
+import { redirect } from "next/navigation";
+
+export async function checkoutCredits(transaction: CheckoutTransactionParams) {
+    const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
+
+    const amount = Number(transaction.amount) * 100;
+
+    const session = await stripe.checkout.sessions.create({
+        line_items: [
+            {
+                price_data: {
+                    currency: 'usd',
+                    unit_amount: amount,
+                    product_data: {
+                        name: transaction.plan,
+                    }
+                },
+                quantity: 1
+            }
+        ],
+        metadata: {
+            plan: transaction.plan,
+            credits: transaction.credits,
+            buyerId: transaction.buyerId,
+        },
+        mode: 'payment',
+        success_url: `${process.env.NEXT_PUBLIC_SERVER_URL}/profile`,
+        cancel_url: `${process.env.NEXT_PUBLIC_SERVER_URL}/`,
+    })
+
+    redirect(session.url!);
+}
 
 export async function createTransactions(transaction: CreateTransactionParams) {
     try {
