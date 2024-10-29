a71fd3af37918b3ff4b4aac6d435342a0e92c7fe
Jatin Srivastava <137695680+JatinSri1909@users.noreply.github.com>
2024-10-29 17:29:46 +0530

    Refactor ProfilePage component to include user authentication and redirect to sign-in page if user is not authenticated. Also fetch user data using getUserById function. Add getUserImages function to retrieve images by user, with pagination support.

--- app/(root)/profile/page.tsx
+++ app/(root)/profile/page.tsx
@@ -1,11 +1,17 @@
-import React from 'react'
+import { getUserById } from "@/lib/actions/user.actions";
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+import React from "react";
 
-const ProfilePage = () => {
-  return (
-    <div>
-      ProfilePage
-    </div>
-  )
-}
+const ProfilePage = async ({ searchParams }: SearchParamProps) => {
+  const page = Number(searchParams?.page) || 1;
+  const { userId } = auth();
 
-export default ProfilePage
+  if(!userId) redirect("/sign-in");
+
+  const user = await getUserById(userId);
+
+  return <div>ProfilePage</div>;
+};
+
+export default ProfilePage;
